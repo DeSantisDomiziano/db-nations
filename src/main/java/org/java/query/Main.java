4ab15de3dd862865d49239f0bc9e523681a69cb8
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-09-28 15:24:30 +0200

    first commit

---
+++ src/main/java/org/java/query/Main.java
@@ -0,0 +1,24 @@
+package org.java.query;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		final String url = "jdbc:mysql://localhost:3306/db-nations";
+		final String user = "root";
+		final String password = "root";
+		
+		try (Connection con = DriverManager.getConnection(url, user, password)) {
+			
+			System.out.println("Connessione stabilita correttamente");
+		} catch (Exception e) {
+			
+			System.out.println("Errore di connessione: " + e.getMessage());
+		}
+		
+		System.out.println("\n----------------------------------\n");
+		System.out.println("The end");
+	}
+}
