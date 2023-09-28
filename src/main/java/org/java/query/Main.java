621d444ab82f8ab9bd16e7d6a66eb2778eec24ee
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-09-28 15:40:20 +0200

    m1&m2 done

--- src/main/java/org/java/query/Main.java
+++ src/main/java/org/java/query/Main.java
@@ -2,6 +2,8 @@ package org.java.query;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Main {
 	public static void main(String[] args) {
@@ -12,7 +14,29 @@ public class Main {
 		
 		try (Connection con = DriverManager.getConnection(url, user, password)) {
 			
-			System.out.println("Connessione stabilita correttamente");
+			final String query = " select c.name, c.country_id, c.region_id, c.country_code2 \n "
+							   + " from countries c "
+							   + " order by c.name asc ";
+			
+			PreparedStatement ps = con.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				
+				String name = rs.getString("name");
+				int country_id = rs.getInt("country_id");
+				int region_id = rs.getInt("region_id");
+				String country_code2 = rs.getString("country_code2");
+				
+				System.out.println("name: " + name + "\n");
+				System.out.println("region id: " + region_id);
+				System.out.println("country: " + country_id);
+				System.out.println("country code: " + country_code2);
+
+				
+				System.out.println("\n------------------------------\n");
+			}
+			
 		} catch (Exception e) {
 			
 			System.out.println("Errore di connessione: " + e.getMessage());
